34d9cfbdda5ca9db534bab9925ed400c46b83335
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-26 14:05:37 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,14 @@
 # code your #valid_move? method here
 
 def valid_move(board, index)
-    def position_taken?(board, ind)
-      if board[ind] == "" || board[ind] == " " || board[ind] == nil
+  def position_taken?(board, ind)
+    if board[ind] == "" || board[ind] == " " || board[ind] == nil
       return false
-      else
+    else
       return true
-    end
+  end
 end
+
         execute something
     else
         execute something else

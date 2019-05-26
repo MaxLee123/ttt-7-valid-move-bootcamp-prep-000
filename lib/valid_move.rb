ccc8ff5dba2623ced7fe9e6a65cee8dba043e3c7
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-26 13:52:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,8 @@
 
 def valid_move
     def position_taken?(board, index)
-    if board[index] == "" || board[index] == " " || board[index] == nil
-    return false
+      if board[index] == "" || board[index] == " " || board[index] == nil
+      return false
   else
     return true
   end
